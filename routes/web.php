82853894e36ba0970f5e8a1b54ffa483e89d658d
Abdulrahman Muhahmed <abdulrahman.elnegery@gmail.com>
2024-12-08 16:18:07 +0200

    updates

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,10 @@
 <?php
 
-use App\Http\Controllers\CommentController;
-use App\Http\Controllers\StatisticController;
-use App\Http\Controllers\TaskController;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\TaskController;
+use App\Http\Controllers\CommentController;
+use App\Http\Controllers\StatisticController;
 
 /*
 |--------------------------------------------------------------------------
