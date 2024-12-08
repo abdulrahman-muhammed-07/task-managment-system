601230b18311c76ea8fef20f35f5a42e64fc901d
Abdulrahman Muhahmed <abdulrahman.elnegery@gmail.com>
2024-12-08 15:11:21 +0200

    updates

--- app/Models/Task.php
+++ app/Models/Task.php
@@ -39,22 +39,22 @@ class Task extends Model
 
     protected static function booted()
     {
-        // Notification when task is created and assigned
-        static::created(function ($task) {
-            if ($task->assignedTo) {
-                $task->assignedTo->notify(
-                    new TaskNotification($task, 'assigned')
-                );
-            }
-        });
+        // // Notification when task is created and assigned
+        // static::created(function ($task) {
+        //     if ($task->assignedTo) {
+        //         $task->assignedTo->notify(
+        //             // new TaskNotification($task, 'assigned')
+        //         );
+        //     }
+        // });
 
-        // Notification when task status is updated
-        static::updated(function ($task) {
-            if ($task->wasChanged('status') && $task->assignedTo) {
-                $task->assignedTo->notify(
-                    new TaskNotification($task, 'status_updated')
-                );
-            }
-        });
+        // // Notification when task status is updated
+        // static::updated(function ($task) {
+        //     if ($task->wasChanged('status') && $task->assignedTo) {
+        //         $task->assignedTo->notify(
+        //             // new TaskNotification($task, 'status_updated')
+        //         );
+        //     }
+        // });
     }
 }
--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -4,18 +4,13 @@ namespace Database\Seeders;
 
 use App\Models\Task;
 use App\Models\User;
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
-use Illuminate\Support\Str;
 
 class UserSeeder extends Seeder
 {
     public function run(): void
     {
-        // ini_set('memory_limit', '1G');
-        // set_time_limit(0);
-
         $userCount = 3;
         $tasksPerUser = 100;
 
